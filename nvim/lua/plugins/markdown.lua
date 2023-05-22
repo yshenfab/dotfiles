e11cdfb2953b6657bc0ba4aa46ce3e787b70d548
yshenfab <yshenfab@gmail.com>
2023-05-22 14:44:30 +0800

    add markdown-toc

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -6,6 +6,11 @@ return {
     build = "cd app && yarn install",
     config = function()
       vim.cmd([[nmap <C-p> :MarkdownPreviewToggle<cr>]])
-    end
+    end,
+  },
+  -- markdown-toc
+  -- :GenTocGFM   GFM (GitHub Flavored Markdown)
+  {
+    "mzlogin/vim-markdown-toc",
   },
 }
