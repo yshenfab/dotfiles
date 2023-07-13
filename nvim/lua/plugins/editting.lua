892dc58a6993c6c73f9af8c3f79243b4dac586c8
yshenfab <yshenfab@gmail.com>
2023-07-13 21:24:40 +0800

    update vim motion with flash.nvim

--- nvim/lua/plugins/editting.lua
+++ nvim/lua/plugins/editting.lua
@@ -29,40 +29,82 @@ return {
 		end,
 	},
 
-	-- [[ Motions: leap, filt, hop (easymotion-like) ]]
+	-- [[ Motions: flash, leap, filt, hop (easymotion-like) ]]
+	-- flash
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {},
+		-- stylua: ignore
+		keys = {
+			{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+			{
+				"S",
+				mode = { "n", "o", "x" },
+				function() require("flash").treesitter() end,
+				desc =
+				"Flash Treesitter"
+			},
+			{
+				"r",
+				mode = "o",
+				function() require("flash").remote() end,
+				desc =
+				"Remote Flash"
+			},
+			{
+				"R",
+				mode = { "o", "x" },
+				function() require("flash").treesitter_search() end,
+				desc =
+				"Treesitter Search"
+			},
+			{
+				"<c-s>",
+				mode = { "c" },
+				function() require("flash").toggle() end,
+				desc =
+				"Toggle Flash Search"
+			},
+		},
+	},
+
 	-- Leap: jump to any positions in the visible editor area by entering a 2-char search
 	-- s/S/gs{c1}{c2}
-	{
-		"ggandor/leap.nvim",
-		keys = {
-			{ "s",  mode = { "n", "x", "o" }, desc = "Leap forward to" },
-			{ "S",  mode = { "n", "x", "o" }, desc = "Leap backward to" },
-			{ "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
-		},
-		config = function(_, opts)
-			local leap = require("leap")
-			for k, v in pairs(opts) do
-				leap.opts[k] = v
-			end
-			leap.add_default_mappings(true)
-			vim.keymap.del({ "x", "o" }, "x")
-			vim.keymap.del({ "x", "o" }, "X")
-		end,
-	},
+	-- {
+	-- 	"ggandor/leap.nvim",
+	-- 	keys = {
+	-- 		{ "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+	-- 		{ "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
+	-- 		{ "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+	-- 	},
+	-- 	config = function(_, opts)
+	-- 		local leap = require("leap")
+	-- 		for k, v in pairs(opts) do
+	-- 			leap.opts[k] = v
+	-- 		end
+	-- 		leap.add_default_mappings(true)
+	-- 		vim.keymap.del({ "x", "o" }, "x")
+	-- 		vim.keymap.del({ "x", "o" }, "X")
+	-- 	end,
+	-- },
+
 	-- filt: enhanced f/t motions for leap, 1-char search
 	-- f/F/t/T{char}
-	{
-		"ggandor/flit.nvim",
-		keys = function()
-			---@type LazyKeys[]
-			local ret = {}
-			for _, key in ipairs({ "f", "F", "t", "T" }) do
-				ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
-			end
-			return ret
-		end,
-		opts = { labeled_modes = "nx" },
-	},
+	-- {
+	-- 	"ggandor/flit.nvim",
+	-- 	keys = function()
+	-- 		---@type LazyKeys[]
+	-- 		local ret = {}
+	-- 		for _, key in ipairs({ "f", "F", "t", "T" }) do
+	-- 			ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
+	-- 		end
+	-- 		return ret
+	-- 	end,
+	-- 	opts = { labeled_modes = "nx" },
+	-- },
+
 	-- hop: EasyMotion-like plugin
 	-- {
 	-- 	"phaazon/hop.nvim",
